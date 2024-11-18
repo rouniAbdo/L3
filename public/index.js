2c5731749be1fd1dbd4e3831b92de1b770eca7b6
Rouni Abdo <ra223aa@student.lnu.se>
2024-11-18 15:35:25 +0100

    Refactor: use server.js, add index.js, and modals for edit/delete.

---
+++ public/index.js
@@ -0,0 +1,5 @@
+import { ProductApp } from '../src/app/productApp.js'
+// Initialize the ProductApp class when the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    new ProductApp()
+})
