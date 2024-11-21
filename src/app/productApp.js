01b94497f8eaafe55f5e4640ed8c16f0edbe3c8d
Rouni Abdo <ra223aa@student.lnu.se>
2024-11-21 16:39:09 +0100

    use LocalStorageHandler for product data and update setup

--- src/app/productApp.js
+++ src/app/productApp.js
@@ -2,25 +2,46 @@ import { ProductManagement } from '../utils/productManagement.js'
 import { ProductEditor } from './productEditor.js'
 import { ProductListView } from './productListView.js'
 import { ProductFormHandler } from './ProductFormHandler.js'
+import { LocalStorageHandler } from './localStorageHandler.js'
 
 export class ProductApp {
-    constructor() {
-        this.productManagement = new ProductManagement()
-    }
+  constructor() {
+    this.productManagement = new ProductManagement()
+    this.localStorageHandler = new LocalStorageHandler(this.productManagement)
+    this.localStorageKey = 'products'
 
-    initializeApp() {
-        const productEditor = new ProductEditor(this.productManagement)
-        const productListView = new ProductListView(this.productManagement, productEditor)
+    this.productEditor = new ProductEditor(this.productManagement)
+    this.productListView = new ProductListView(this.productManagement, this.productEditor)
+    this.productEditor.productListView = this.productListView
+    this.productFormHandler = new ProductFormHandler(this.productManagement, this.productListView)
+  }
 
-        // update the productEditor with the productListView
-        productEditor.productListView = productListView
+  /**
+   * This method is called to initialize the app after the DOM has loaded.
+   */
+  initializeApp() {
+    // Load products from localStorage if they exist
+    this.localStorageHandler.loadFromLocalStorage()
 
-        // create a new instance of ProductFormHandler
-        const productFormHandler = new ProductFormHandler(
-            this.productManagement, 
-            productListView
-        )
-        // show the initial view
-        productListView.updateView()
-    }
+    // Update the view with the current products
+    this.productListView.updateView()
+
+    // Hook methods to save to localStorage when changes occur
+    this.hookProductManagementMethods()
+  }
+
+  /**
+   * Hooks methods to save to localStorage when products are changed.
+   */
+  hookProductManagementMethods() {
+    ['addProduct', 'removeProduct', 'updateProduct'].forEach(method => {
+      const originalMethod = this.productManagement[method].bind(this.productManagement)
+      this.productManagement[method] = (...args) => {
+        const result = originalMethod(...args)
+
+        this.localStorageHandler.saveToLocalStorage()
+        return result
+      }
+    })
+  }
 }
