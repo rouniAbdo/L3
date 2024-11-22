822415233dcdda1a6cc2141da5f9ce7827a4cc84
Rouni Abdo <ra223aa@student.lnu.se>
2024-11-22 10:41:03 +0100

    Refactor server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+
+
 const app = express()
 const port = 3000
+const __dirname = path.resolve()
 // serve the public directory
-app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use('/src', express.static(path.join(__dirname, '..', 'src')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/src', express.static(path.join(__dirname, 'src')))
 // serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+  res.sendFile(path.join(__dirname,  'public', 'index.html'))
 })
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
